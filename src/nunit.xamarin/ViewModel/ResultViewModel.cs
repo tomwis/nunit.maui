a22f527da46b384021641b346becc40d50bd80f1
Tomasz Wiśniewski <tomaszwisniewski00@gmail.com>
2023-10-16 10:40:18 +0200

    Added duration in ms and wrapping of parent type to list view.

--- src/nunit.xamarin/ViewModel/ResultViewModel.cs
+++ src/nunit.xamarin/ViewModel/ResultViewModel.cs
@@ -24,6 +24,7 @@
 using System.Reflection;
 using System.Windows.Input;
 using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
 using NUnit.Runner.Extensions;
 using NUnit.Runner.Helpers;
 
@@ -31,6 +32,7 @@ namespace NUnit.Runner.ViewModel
 {
     internal class ResultViewModel
     {
+        private const double MillisecondsInSecond = 1000.0;
         private readonly List<Assembly> _testAssemblies;
 
         public ResultViewModel(ITestResult result, List<Assembly> testAssemblies)
@@ -39,6 +41,11 @@ namespace NUnit.Runner.ViewModel
             TestResult = result;
             Name = result.Name;
             Parent = result.Test.Parent.FullName;
+            if (result is TestCaseResult testCaseResult)
+            {
+                DurationMs = testCaseResult.Duration * MillisecondsInSecond;
+            }
+            
             Result = result.ResultState.Status.ToString().ToUpperInvariant();
             Message = result.Message;
         }
@@ -47,6 +54,7 @@ namespace NUnit.Runner.ViewModel
         public string Result { get; set; }
         public string Name { get; private set; }
         public string Parent { get; private set; }
+        public double DurationMs { get; private set; }
         public string Message { get; private set; }
         public IReadOnlyList<Assembly> TestAssemblies => _testAssemblies.AsReadOnly();
 
